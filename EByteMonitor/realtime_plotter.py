83d96bd2c2ac45bf8fe5cf7ce9525289bf798312
infernoDison <disonwu8@gmail.com>
2020-02-05 20:12:57 -0500

    <UPDATE> Changes in the visualizer
    Minor changes for usability

--- EByteMonitor/realtime_plotter.py
+++ EByteMonitor/realtime_plotter.py
@@ -3,21 +3,19 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import networkx as nx
 import serial
-import threading
 import time
-import json
 import binascii
 import sys
-import threading
 from datetime import datetime
 from networkx.drawing.nx_agraph import write_dot, graphviz_layout
 
-# Create figure for plotting
-fig = plt.figure()
-
+# A list of nodes in the network
 node_list=[]
 
-topology_data = {}
+# A dictionary for keeping the connection and connection timestamp for plotting
+edge_labels={}
+
+G = nx.Graph()
 
 MESSAGE_TYPE_STRING = ["MESSAGE_JOIN", "MESSAGE_JOIN_ACK", "MESSAGE_JOIN_CFM",
                        "MESSAGE_CHECK_ALIVE", "MESSAGE_REPLY_ALIVE",
@@ -31,12 +29,26 @@ TYPE_MESSAGE_REPLY_ALIVE = 5
 TYPE_MESSAGE_GATEWAY_REQ = 6
 TYPE_MESSAGE_NODE_REPLY = 7
 
-MY_NODE_SIZE = 1200
-
 # Debugging message only used in special tests
 TYPE_MESSAGE_MULTIHOP = 8
 
-G = nx.DiGraph()
+MY_NODE_SIZE = 1200
+
+def plot():
+    #Map the color (Gateway has a different color)
+    color_map = []
+    for node in G:
+        if int(node, 16) > 0x8000:
+            #This is a gateway
+            color_map.append('g')
+        else:
+            color_map.append('y')
+
+    plt.clf()
+    write_dot(G,'test.dot')
+    pos=graphviz_layout(G, prog='neato')
+    nx.draw(G,pos, with_labels=True, node_color=color_map, node_size = MY_NODE_SIZE)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.5)
 
 # This function is called periodically from FuncAnimation
 def animate(i, ser, file):
@@ -44,6 +56,7 @@ def animate(i, ser, file):
     if ser.in_waiting > 0:
         c = ser.read(1)
     else:
+        plot()
         return
 
     msg_type = int.from_bytes(c, 'big')
@@ -71,6 +84,8 @@ def animate(i, ser, file):
     log_file.write(log_string)
     log_file.write('\n')
 
+    edge = (src_addr, dest_addr)
+
     # Start to process the message
     if msg_type == TYPE_MESSAGE_JOIN or msg_type == TYPE_MESSAGE_JOIN_ACK:
 
@@ -91,40 +106,51 @@ def animate(i, ser, file):
         node_info['time_joined'] = current_time
         node_info['parent_last_alive'] = current_time
 
-        # Save the information for this node
-        topology_data[src_addr] = node_info
+        edge_labels[edge] = 'Connected: ' + current_time
+
         G.add_edge(src_addr, dest_addr)
 
     elif msg_type == TYPE_MESSAGE_REPLY_ALIVE:
         # Receiving a REPLY_ALIVE message means that the dest node has confirmed its aliveness to the src node
-        if dest_addr in topology_data:
-            topology_data[dest_addr]['parent_last_alive'] = current_time
-        else:
-            print(str(dest_addr) + " is not in the record")
+        if edge_labels.get(edge) == None:
+            # This connection has never been seen before, add it to the graph
+            node_list.append(src_addr)
+            node_list.append(dest_addr)
 
-    #Map the color (Gateway has a different color)
-    color_map = []
-    for node in G:
-        if int(node, 16) > 0x8000:
-            #This is a gateway
-            color_map.append('g')
-        else:
-            color_map.append('y')
+            G.add_edge(src_addr, dest_addr)
 
-    plt.clf()
-    write_dot(G,'test.dot')
-    pos=graphviz_layout(G, prog='dot')
-    nx.draw(G,pos, with_labels=True, node_color=color_map, node_size = MY_NODE_SIZE)
+        edge_labels[edge] = 'Connected: ' + current_time
 
-if len(sys.argv) < 2:
-    print("Please provide the serial port path")
-    exit(1)
+    # Update the plot
+    plot()
 
-port = sys.argv[1]
-print("Try to open serial port: " + port)
-ser = serial.Serial(port, 9600, timeout=1)
-log_file =open("log.txt", "w")
 
-# Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(ser, log_file), interval=1000)
-plt.show()
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 2:
+        print("Usage: realtime_plotter.py <serial_port_path> [gateway address]")
+        exit(1)
+
+    port = sys.argv[1]
+
+    print("Try to open serial port: " + port)
+    ser = serial.Serial(port, 9600, timeout=1)
+    log_file =open("log.txt", "w")
+
+    gateway = "FFFF"
+    if len(sys.argv) == 3:
+        gateway = sys.argv[2]
+        print("User-defined gateway is 0x" + gateway)
+        node_list.append(gateway)
+        G.add_node(gateway)
+
+
+    # Create figure for plotting
+    fig = plt.figure(figsize=(12,9))
+    plt.title("LoRa Multi-hop Network Visualizer")
+    plot()
+
+    # Set up plot to call animate() function periodically
+    ani = animation.FuncAnimation(fig, animate, fargs=(ser, log_file), interval=1000)
+    plt.show()
