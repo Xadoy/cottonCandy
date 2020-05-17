7a8a9e16886e0a2538e13fe8c49e5c90372b03dc
Dison <disonwu8@gmail.com>
2020-05-16 23:42:29 -0400

    <UPDATE> Organized the examples
    Removed all the test programs from the repository. Add Gateway.ino and Node.ino with explanations for general usage.

---
+++ examples/Gateway/Gateway.ino
@@ -0,0 +1,78 @@
+#include <LoRaMesh.h>
+#include <EbyteDeviceDriver.h>
+// Uncomment the next line for using Adafruit LoRa Feather
+// #include <AdafruitDeviceDriver.h>
+
+/**
+ * Define pins for connection with Ebyte LoRa transceiver
+ */ 
+#define LORA_RX 10
+#define LORA_TX 11
+#define LORA_M0 2
+#define LORA_M1 3
+#define LORA_AUX 4
+
+// The time (milliseconds) between gateway requesting data
+// Currently set to every 300 seconds (5 minutes)
+#define GATEWAY_REQ_TIME 300000
+
+LoRaMesh *manager;
+
+DeviceDriver *myDriver;
+
+// 2-byte long address 
+// For Gateway only: The first bit of the address has to be 1
+byte myAddr[2] = {0x80, 0xA0};
+
+/**
+ * Callback function that will be called when Gateway receives the reply from a node
+ */
+void onReciveResponse(byte *data, byte len)
+{
+
+  // In the example, we simply print out the reply message from the node
+  Serial.print(F("Gateway received a node reply: "));
+
+  for (int i = 0; i < len; i++)
+  {
+    Serial.print((data[i]), HEX);
+  }
+  Serial.println(".");
+}
+
+void setup()
+{
+  Serial.begin(57600);
+
+  // Wait for serial port to connect.
+  while (!Serial)
+  {
+    ; 
+  }
+
+  // In this example, we will be using an EByte E22 LoRa transceiver
+  myDriver = new EbyteDeviceDriver(LORA_RX, LORA_TX, LORA_M0, LORA_M1, LORA_AUX, myAddr, 0x09);
+
+  // Uncomment the next line for using Adafruit LoRa Feather
+  // myDriver = new AdafruitDeviceDriver(myAddr, 0x09);
+
+  // Initialize the driver
+  myDriver->init();
+
+  // Create a LoRaMesh object
+  manager = new LoRaMesh(myAddr, myDriver);
+
+  // For Gateway only: Set up the time interval between requests
+  manager->setGatewayReqTime(GATEWAY_REQ_TIME);
+
+  // Set up the callback funtion
+  manager->onReceiveResponse(onReciveResponse);
+}
+
+void loop()
+{
+  Serial.println("Loop starts");
+
+  // Start the gateway
+  manager->run();
+}
---
+++ examples/Node/Node.ino
@@ -0,0 +1,78 @@
+#include <LoRaMesh.h>
+#include <EbyteDeviceDriver.h>
+// Uncomment the next line for using Adafruit LoRa Feather
+// #include <AdafruitDeviceDriver.h>
+
+/**
+ * Define pins for connection with Ebyte LoRa transceiver
+ */
+#define LORA_RX 10
+#define LORA_TX 11
+#define LORA_M0 2
+#define LORA_M1 3
+#define LORA_AUX 4
+
+LoRaMesh *manager;
+
+DeviceDriver *myDriver;
+
+// 2-byte long address
+// For regular nodes: The first bit of the address is 0
+byte myAddr[2] = {0x00, 0xA0};
+
+/**
+ * Callback function that will be called when the node receives the request from the gateway
+ * and needs to reply back
+ */
+void onReceiveRequest(byte **data, byte *len)
+{
+
+  // In the example, we simply send a series of HEX number of "ABCDEF" followed with the least significant byte of the node address
+  Serial.println("onReciveRequest callback");
+  (*data)[0] = 0xA;
+  (*data)[1] = 0xB;
+  (*data)[2] = 0xC;
+  (*data)[3] = 0xD;
+  (*data)[4] = 0xE;
+  (*data)[5] = 0xF;
+  (*data)[6] = 0xA;
+  (*data)[7] = 0x0;
+  *len = 0x08;
+  for (int i = 0; i < *len; i++)
+  {
+    Serial.print(((*data)[i]), HEX);
+  }
+  Serial.print('\n');
+}
+
+void setup()
+{
+  Serial.begin(57600);
+
+  // Wait for serial port to connect.
+  while (!Serial)
+  {
+    ;
+  }
+
+  // In this example, we will be using an EByte E22 LoRa transceiver
+  myDriver = new EbyteDeviceDriver(LORA_RX, LORA_TX, LORA_M0, LORA_M1, LORA_AUX, myAddr, 0x09);
+
+  // Uncomment the next line for using Adafruit LoRa Feather
+  // myDriver = new AdafruitDeviceDriver(myAddr, 0x09);
+  myDriver->init();
+
+  // Create a LoRaMesh object
+  manager = new LoRaMesh(myAddr, myDriver);
+
+  // Set up the callback funtion
+  manager->onReceiveRequest(onReceiveRequest);
+}
+
+void loop()
+{
+  Serial.println("Loop starts");
+
+  // Start the node
+  manager->run();
+}
